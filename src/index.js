0cf732082db4d077e2e3adc4080a1b2d07d5c427
Daniel Whiffing <danielwhiffing@gmail.com>
2019-04-23 17:35:49 -0400

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './Tabs'
+import Tabs from './Tabs'
 import { StoreProvider } from './storeContext'
 
 ReactDOM.render(
   <StoreProvider>
-    <App />
+    <Tabs />
   </StoreProvider>,
   document.getElementById('root')
 )
